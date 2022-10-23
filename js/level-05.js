e5bfea079a29e53a6cc214256544a3883366cb7a
Vlad Shchelkunov <specter.ode@gmail.com>
2022-10-23 22:13:11 +0300

    +

--- js/level-05.js
+++ js/level-05.js
@@ -1348,3 +1348,110 @@
 // console.log("removeNb(26): ", removeNb(26)); //return [15,21], [21, 15]
 // console.log("removeNb(1000003): ", removeNb(1000003)); // [550320, 908566], [559756, 893250], [893250, 559756], [908566, 550320]
 // ----------------------------------------------------------------------
+// Count IP Addresses
+// Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
+// All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
+
+// function ipsBetween(start, end) {
+//   const sum = (ip) =>
+//     ip.split(".").reduce((acc, n) => acc * 256 + Number(n), 0);
+//   return sum(end) - sum(start);
+// }
+// console.log(
+//   'ipsBetween("170.0.0.0", "170.1.0.0", 65536);: ',
+//   ipsBetween("170.0.0.0", "170.1.0.0")
+// );
+// console.log(
+//   'ipsBetween("20.0.0.10", "20.0.1.0", 246);: ',
+//   ipsBetween("20.0.0.10", "20.0.1.0")
+// );
+// console.log(
+//   'ipsBetween("10.11.12.13", "10.11.13.0", 243);: ',
+//   ipsBetween("10.11.12.13", "10.11.13.0")
+// );
+
+// ----------------------------------------------------------------------
+// A Chain adding function
+// We want to create a function that will add numbers together when called in succession.
+// add(1)(2); // == 3
+// We also want to be able to continue to add numbers to our chain.
+// add(1)(2)(3); // == 6
+// add(1)(2)(3)(4); //  == 10
+// add(1)(2)(3)(4)(5); // == 15
+// and so on.
+// A single call should be equal to the number passed in.
+// add(1); // == 1
+// We should be able to store the returned values and reuse them.
+
+// const addTwo = add(2);
+// addTwo; // == 2
+// addTwo + 5; // == 7
+// addTwo(3); // == 5
+// addTwo(3)(5); // == 10
+// We can assume any number being passed in will be valid whole number.
+
+// function add(n) {
+//   const result = (x) => add(n + x);
+//   result.valueOf = () => n;
+//   return result;
+// }
+
+// console.log("add(1)(2)(3): -- 6", add(1)(2)(3));
+// console.log("add(1)(2)(3)(4)(5), 15", add(1)(2)(3)(4)(5));
+// ----------------------------------------------------------------------
+
+// Josephus Permutation
+// This problem takes its name by arguably the most important event in the life of the ancient historian Josephus: according to his tale, he and his 40 soldiers were trapped in a cave by the Romans during a siege.
+// Refusing to surrender to the enemy, they instead opted for mass suicide, with a twist: they formed a circle and proceeded to kill one man every three, until one last man was left (and that it was supposed to kill himself to end the act).
+// Well, Josephus and another man were the last two and, as we now know every detail of the story, you may have correctly guessed that they didn't exactly follow through the original idea.
+// You are now to create a function that returns a Josephus permutation, taking as parameters the initial array/list of items to be permuted as if they were in a circle and counted out every k places until none remained.
+// Tips and notes: it helps to start counting from 1 up to n, instead of the usual range 0..n-1; k will always be >=1.
+// For example, with n=7 and k=3 josephus(7,3) should act this way.
+
+// [1,2,3,4,5,6,7] - initial sequence
+// [1,2,4,5,6,7] => 3 is counted out and goes into the result [3]
+// [1,2,4,5,7] => 6 is counted out and goes into the result [3,6]
+// [1,4,5,7] => 2 is counted out and goes into the result [3,6,2]
+// [1,4,5] => 7 is counted out and goes into the result [3,6,2,7]
+// [1,4] => 5 is counted out and goes into the result [3,6,2,7,5]
+// [4] => 1 is counted out and goes into the result [3,6,2,7,5,1]
+// [] => 4 is counted out and goes into the result [3,6,2,7,5,1,
+
+function josephus(items, step) {
+  const result = [];
+  let i = 0;
+  let k = 1;
+  if (items.length < 1) return items;
+  while (items.length !== 1) {
+    if (k === step && i === items.length - 1) {
+      result.push(items[i]);
+      items.splice(i, 1);
+      i = 0;
+      k = 1;
+    } else if (k === step && i != items.length - 1) {
+      result.push(items[i]);
+      items.splice(i, 1);
+      k = 1;
+    } else if (k < step && i === items.length - 1) {
+      k++;
+      i = 0;
+    } else if (k < step && i !== items.length - 1) {
+      k++;
+      i++;
+    }
+  }
+  result.push(items[0]);
+  return result;
+}
+// console.log(
+//   "josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2): ",
+//   josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)
+// );
+console.log(
+  'josephus (["C", "o", "d", "e", "W", "a", "r", "s"], 4): ',
+  josephus(["C", "o", "d", "e", "W", "a", "r", "s"], 4)
+); //['e', 's', 'W', 'o', 'C', 'd', 'r', 'a'])
+// console.log(
+//   "josephus ([ 1, 2, 3, 4, 5, 6, 7 ], 3): ",
+//   josephus([1, 2, 3, 4, 5, 6, 7], 3)
+// );
